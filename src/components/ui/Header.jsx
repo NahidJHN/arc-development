939f7da04ce11b203160c2032e2c6ca1b5b429fa
NahidJHN <jubaidhossain66@gmail.com>
2022-03-12 01:06:06 +0600

    start a new project

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Tabs from '@mui/material/Tabs'
+
+const Header = () => {
+    return (
+        <AppBar position="sticky" color="primary">
+            <Toolbar>
+                <Typography variant="h6">
+                    Arc Development
+                </Typography>
+                <AppBar position="static" color="primary">
+                  <Tabs value={} onChange={} aria-label="">
+                    
+                  </Tabs>
+                </AppBar>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
